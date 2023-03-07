78d50ebcaa6e29f1bf358ce151409410b2ea3909
kristinetuazon <kristine.jane.tuazon@gmail.com>
2023-03-07 17:50:13 +0900

    feat: add all routes/installed prosidebar

--- dashboard-ui/src/App.tsx
+++ dashboard-ui/src/App.tsx
@@ -1,9 +1,21 @@
 // @ts-nocheck
-import { useState } from 'react'
 import './App.css'
 import { ColorModeContext, useMode } from './theme'
 import { CssBaseline, ThemeProvider } from '@mui/material'
-import TopBar from './components/TopBar';
+import { Routes, Route } from 'react-router-dom';
+import TopBar from './scenes/global/TopBar';
+import SideBar from './scenes/global/SideBar';
+import Dashboard from './scenes/Dashboard';
+// import Team from './scenes/Team';
+// import Invoices from './scenes/Invices';
+// import Contact from './scenes/Contact';
+// import Bar from './scenes/Bar';
+// import Form from './scenes/Form';
+// import Line from './scenes/Line';
+// import Pie from './scenes/Pie';
+// import FAQ from './scenes/FAQ';
+// import Geography from './scenes/Geography';
+// import Calendar from './scenes/Calendar';
 
 
 function App() {
@@ -16,6 +28,18 @@ function App() {
     <div className="app">
       <main className="content">
         <TopBar/>
+        <Routes>
+          <Route path="/" element={<Dashboard/>}/>
+          {/* <Route path="/contacts" element={<Contacts/>}/>
+          <Route path="/invoices" element={<Invoices/>}/>
+          <Route path="/form" element={<Form/>}/>
+          <Route path="/bar" element={<Bar/>}/>
+          <Route path="/pie" element={<Pie/>}/>
+          <Route path="/line" element={<Line/>}/>
+          <Route path="/faq" element={<FAQ/>}/>
+          <Route path="/geography" element={<Geography/>}/>
+          <Route path="/calendar" element={<Calendar/>}/> */}
+        </Routes>
       </main>
     </div>
     </ThemeProvider>
---
+++ dashboard-ui/src/scenes/Dashboard.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Sidebar, Menu, MenuItem} from 'react-pro-sidebar'
+
+type Props = {}
+
+const Dashboard = (props: Props) => {
+  return (
+    <div>Dashboard</div>
+  )
+}
+
+export default Dashboard
