d9fa3079f5e755edd43f7294fa5c5a6ea52f2928
kristinetuazon <kristine.jane.tuazon@gmail.com>
2023-03-16 19:26:34 +0900

    feat: dashboard/header

--- dashboard-ui/src/scenes/Dashboard.tsx
+++ dashboard-ui/src/scenes/Dashboard.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
-
+import Header from '../components/Header'
+import { Box } from '@mui/material'
 
 type Props = {}
 
 const Dashboard = (props: Props) => {
   return (
-    <div>Dashboard</div>
+    <Box m="20px">
+      <Box display="flex" justifyContent="spaceBetween" alignItems="center">
+      <Header title="HEADER" subtitle="Welcome"/>
+      </Box>
+    </Box>
   )
 }
 
