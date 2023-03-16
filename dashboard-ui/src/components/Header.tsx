ff1526bb51490295e05b9bf11e99c71446d2a7ec
kristinetuazon <kristine.jane.tuazon@gmail.com>
2023-03-16 19:20:09 +0900

    feat: header componenet

--- dashboard-ui/src/components/Header.tsx
+++ dashboard-ui/src/components/Header.tsx
@@ -2,11 +2,21 @@ import React from 'react'
 import { Typography, Box, useTheme } from '@mui/material'
 import { tokens } from '../theme';
 
-type Props = {}
+type Props = {
+  title?: string,
+  subtitle?: string,
+}
 
 const Header = (props: Props) => {
+  const {title, subtitle} = props;
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
   return (
-    <div>Header</div>
+    <Box mb="30px">
+      <Typography variant="h2" color={colors.grey[100]} fontWeight="bold" sx={{mb: "5px"}}>{title}</Typography>
+      <Typography variant="h5" color={colors.greenAccent[400]}>{subtitle}</Typography>
+    </Box>
   )
 }
 
