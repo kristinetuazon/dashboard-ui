ddaaf7fdd183061730bfdd1a7fe88f5c498e65d4
kristinetuazon <kristine.jane.tuazon@gmail.com>
2023-03-30 12:24:48 +0900

    feat: calendar

--- dashboard-ui/src/App.tsx
+++ dashboard-ui/src/App.tsx
@@ -11,12 +11,12 @@ import Team from "./scenes/team";
 import Contacts from "./scenes/contacts";
 import Invoices from "./scenes/invoices";
 import Form from "./scenes/form";
+import Calendar from './scenes/calendar';
 // import Bar from './scenes/Bar';
 // import Line from './scenes/Line';
 // import Pie from './scenes/Pie';
 // import FAQ from './scenes/FAQ';
 // import Geography from './scenes/Geography';
-// import Calendar from './scenes/Calendar';
 //  import { ProSidebarProvider } from 'react-pro-sidebar';
 
 function App() {
@@ -37,12 +37,12 @@ function App() {
               <Route path="/contacts" element={<Contacts />} />
               <Route path="/invoices" element={<Invoices />} />
               <Route path="/form" element={<Form />} />
+              <Route path="/calendar" element={<Calendar/>}/> 
               {/* <Route path="/bar" element={<Bar/>}/>
           <Route path="/pie" element={<Pie/>}/>
           <Route path="/line" element={<Line/>}/>
           <Route path="/faq" element={<FAQ/>}/>
-          <Route path="/geography" element={<Geography/>}/>
-          <Route path="/calendar" e®lement={<Calendar/>}/> */}
+          <Route path="/geography" element={<Geography/>}/>*/}
             </Routes>
           </main>
         </div>
