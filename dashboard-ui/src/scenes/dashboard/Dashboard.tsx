3b167d257afe8250ff5d9ec37f431fbdd42bf51b
kristinetuazon <kristine.jane.tuazon@gmail.com>
2023-04-04 17:26:18 +0900

    fix: some layout issues

--- dashboard-ui/src/scenes/dashboard/Dashboard.tsx
+++ dashboard-ui/src/scenes/dashboard/Dashboard.tsx
@@ -25,7 +25,7 @@ const Dashboard = (props: Props) => {
   const colors = tokens(theme.palette.mode);
 
   return (
-    <Box m="20px">
+    <Box m="20px" overflow="auto" height="90%" width="100%" sx={{overflowX:"hidden"}}>
       <Box display="flex" justifyContent="space-between" alignItems="center">
         <Header title="DASHBOARD" subtitle="Welcome to your dashboard" />
 
@@ -185,12 +185,45 @@ const Dashboard = (props: Props) => {
 
 <Box display="flex" justifyContent="space-between" alignItems="center" borderBottom={`4px solid ${colors.primary[500]}`} color={colors.grey[100]} p='15px'>
 
-<Typography>
+<Typography color={colors.grey[100]} variant="h5" fontWeight="600">
   Recent Transactions
 </Typography>
 </Box>
 
+{mockTransactions.map((transaction:any, i:number) => (
+            <Box
+              key={`${transaction.txId}-${i}`}
+              display="flex"
+              justifyContent="space-between"
+              alignItems="center"
+              borderBottom={`4px solid ${colors.primary[500]}`}
+              p="15px"
+            >
+              <Box>
+                <Typography
+                  color={colors.greenAccent[500]}
+                  variant="h5"
+                  fontWeight="600"
+                >
+                  {transaction.txId}
+                </Typography>
+                <Typography color={colors.grey[100]}>
+                  {transaction.user}
+                </Typography>
+              </Box>
+              <Box color={colors.grey[100]}>{transaction.date}</Box>
+              <Box
+                sx={{backgroundColor: colors.greenAccent[500]}}
+                p="5px 10px"
+                borderRadius="4px"
+              >
+                ${transaction.cost}
+              </Box>
             </Box>
+          ))}
+
+            </Box>
+
         </Box>
       </Box>
     </Box>
